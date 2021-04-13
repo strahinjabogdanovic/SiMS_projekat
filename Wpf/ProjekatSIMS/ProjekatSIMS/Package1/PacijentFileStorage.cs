867c95226c2008ef4a5b8389d201bed2b8f7685c
NatasaVasicRA10 <vasic.ra10.2018@uns.ac.rs>
2021-04-13 18:31:26 +0200

    #wpf
    Popravljeno zakazivanje, otkazivanje i promena termina

--- Wpf/ProjekatSIMS/ProjekatSIMS/Package1/PacijentFileStorage.cs
+++ Wpf/ProjekatSIMS/ProjekatSIMS/Package1/PacijentFileStorage.cs
@@ -126,5 +126,118 @@ namespace Package1
             }
 
         }
+
+
+        public void UpdateSekretar(DataGrid dataGridPacijenti)
+        {
+            int currentRowIndex = dataGridPacijenti.Items.IndexOf(dataGridPacijenti.SelectedItem);
+
+            string tempFile = System.IO.Path.GetTempFileName();
+
+            using (var sr = new StreamReader("termini.txt"))
+            using (var sw = new StreamWriter(tempFile))
+            {
+
+                string line;
+                int id = 0;
+                while ((line = sr.ReadLine()) != null)
+                {
+                    //id++;
+                    if (id == currentRowIndex)
+                    {
+
+                        String[] termin = line.Split('/');
+                        //var pac = new TerminiPacijenata();
+
+
+                        DataGridRow row = (DataGridRow)dataGridPacijenti.ItemContainerGenerator.ContainerFromIndex(currentRowIndex);
+
+                        TextBlock t1 = dataGridPacijenti.Columns[0].GetCellContent(row) as TextBlock;
+                        TextBlock t2 = dataGridPacijenti.Columns[1].GetCellContent(row) as TextBlock;
+                        TextBlock t3 = dataGridPacijenti.Columns[2].GetCellContent(row) as TextBlock;
+                        TextBlock t4 = dataGridPacijenti.Columns[3].GetCellContent(row) as TextBlock;
+                        TextBlock t5 = dataGridPacijenti.Columns[4].GetCellContent(row) as TextBlock;
+
+
+                        sw.WriteLine(t2.Text + "/" + t3.Text + "/" + t4.Text + "/" + t5.Text + "/" + id + "/" + t1.Text);
+                    }
+                    else
+                    {
+                        sw.WriteLine(line);
+                    }
+                    id++;
+                }
+            }
+            File.Delete("termini.txt");
+            File.Move(tempFile, "termini.txt");
+
+        }
+
+
+
+        String[] terminin;
+        public void KreirajSekretar(string lekar, string tb, string tb1, string tb2)
+        {
+
+            string slj = "";
+
+
+            if (lekar != null)
+            {
+
+                if (lekar.Contains("Jova Jovic"))
+                {
+                    slj = "Jova Jovic";
+
+                }
+                else if (lekar.Contains("Jovan Jovanovic"))
+                {
+                    slj = "Jovan Jovanovic";
+                }
+            }
+
+            string tempFile = System.IO.Path.GetTempFileName();
+
+            //String[] terminin;
+
+            using (var srp = new StreamReader("termini.txt"))
+            using (var sw = new StreamWriter(tempFile))
+            {
+                string linep;
+                int idd = 0;
+                while ((linep = srp.ReadLine()) != null)
+                {
+                    terminin = linep.Split('/');
+                    var pacijent = new TerminiPacijenata();
+
+                    idd = int.Parse(terminin[4]);
+                    idd++;
+
+                    sw.WriteLine(linep);
+
+                }
+                string doktor = "";
+                using (var sars = new StreamReader("doktori.txt"))
+                {
+                    string lines;
+                    while ((lines = sars.ReadLine()) != null)
+                    {
+                        String[] termin = lines.Split('/');
+                        lines = termin[0];
+                        if (lines == slj)
+                        {
+                            doktor = termin[1];
+                        }
+
+                    }
+                }
+                sw.WriteLine(tb + "/" + tb1 + "/" + slj + "/" + doktor + "/" + idd + "/" + tb2);
+
+                Console.WriteLine(tb2);
+
+            }
+            File.Delete("termini.txt");
+            File.Move(tempFile, "termini.txt");
+        }
     }
 }
