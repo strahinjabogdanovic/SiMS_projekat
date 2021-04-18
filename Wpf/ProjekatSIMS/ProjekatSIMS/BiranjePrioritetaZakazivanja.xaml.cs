92e3222cba9da41ba4315a9b7554b0b62b0065f2
Iva Đondović <djondovic.ra14.2018@uns.ac.rs>
2021-04-18 11:50:05 +0200

    #prioritetno_zakazivanje
    Dodata opcija prioritetnog zakazivanja

---
+++ Wpf/ProjekatSIMS/ProjekatSIMS/BiranjePrioritetaZakazivanja.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace ProjekatSIMS
+{
+    /// <summary>
+    /// Interaction logic for BiranjePrioritetaZakazivanja.xaml
+    /// </summary>
+    public partial class BiranjePrioritetaZakazivanja : Window
+    {
+        public BiranjePrioritetaZakazivanja()
+        {
+            InitializeComponent();
+        }
+
+        private void Date_Checked(object sender, RoutedEventArgs e)
+        {
+            PrioritetDatum pd = new PrioritetDatum();
+            pd.ShowDialog();
+        }
+
+        private void Doctor_Checked(object sender, RoutedEventArgs e)
+        {
+            PrioritetLekar pl = new PrioritetLekar();
+            pl.ShowDialog(); 
+        }
+    }
+}
